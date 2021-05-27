b363331759175fb489c6567a1e84ac177d803976
lucky <anggara.lucky1992@gmail.com>
2021-05-27 15:19:10 +0700

    kantor

--- app/Http/Controllers/PegawaiController.php
+++ app/Http/Controllers/PegawaiController.php
@@ -9,7 +9,11 @@ use App\Models\Pegawai;
 class PegawaiController extends Controller
 {
     public function index(){
-        $data = Pegawai::all();
+        $data = Pegawai::
+        select('master_pegawai.*','master_jabatan.nama as jabatan','master_divisi.nama as divisi')
+        ->join('master_jabatan','master_pegawai.jabatan_id','=','master_jabatan.id')
+        ->join('master_divisi','master_pegawai.divisi_id','=','master_divisi.id')
+        ->get();
         return response()->json($data, 200);
     }
 }
--- app/Models/Pegawai.php
+++ app/Models/Pegawai.php
@@ -5,6 +5,7 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\SoftDeletes;
+use Carbon\Carbon;
 
 class Pegawai extends Model
 {
@@ -15,9 +16,5 @@ class Pegawai extends Model
     protected $guarded = [
         'id',
     ];
-
-    public function getFullNameAttribute()
-    {
-        return "{$this->nama_lengkap}";
-    }
 }
+
